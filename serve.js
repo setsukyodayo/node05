a40f2666873f8fbe6aa7d288bd41417955c818d8
setsukyodayo <200098ve@yse-c.net>
2021-12-17 10:05:15 +0900

    a

---
+++ serve.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app =express();
+const http=require('http').createServer(app);
+
+const io = require('soket.io')(http);
+
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: true }))
+
+
+const dotenv=require('dotenv');
+dotenv.config();
+const host= process.env.HOST;
+const port= process.env.PORT;
+
+http .listen(port ,host,()=> {
+    console.log('http://'+host+':'+port);
+})
